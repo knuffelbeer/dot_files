3cb69107b88b4263ce60825264120e425c8e1201
knuffelbeer <beerforbusiness@outlook.com>
2024-06-16 16:42:21 +0200

    symlinks update

--- symlinks.sh
+++ symlinks.sh
@@ -1,3 +1,8 @@
-ln -s ~/dot_files/nvim ~/.config/nvim
-ln -s ~/dot_files/awesome ~/.config/awesome
+configDirectories=("nvim" "awesome" "alacritty")
+
+for dir in "${configDirectories[@]}"; do
+	ln -s "$HOME/dot_files/${dir}" "$HOME/.config/${dir}"
+done
+
+# Files that are not in .config.
 ln -s ~/dot_files/bashScripts ~/bashScripts
