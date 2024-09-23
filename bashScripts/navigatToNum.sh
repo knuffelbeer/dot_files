f034081f55375d2adfa223c80c2fc23caeb21940
knuffelbeer <beerforbusiness@outlook.com>
2024-09-23 23:07:17 +0200

    Algorithm of everything configuration

--- bashScripts/navigatToNum.sh
+++ bashScripts/navigatToNum.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # A kind off hacky way to get workspace-like funcionality for windows in your 
-# session. You can define a custom setup for one of you sessions and have
+# session. It doesn't make you define windows in order of their index, which
+# allows for running programs in set windows, and thus always mapping a program
+# to a window. also you can have custom configuration for sessions. 
 
 
 TARGET_INDEX=$1
@@ -10,17 +12,34 @@ if [ "$TARGET_INDEX" = 0 ];then
 fi
 
 CURRENT_SESSION=$(tmux display-message -p '#S')
-echo "$CURRENT_SESSION"
 
+
+case "$CURRENT_SESSION"  in
 # list here your directories that should have a custom commands mapped to
-# there windows.
-if [ "$CURRENT_SESSION" = "knuffelbeer" ]; then
-		echo "knuffelbeer"
+# as a case with their custom configuration.
+	"AlgorithmOfEverything")
 		if tmux list-windows | grep -q "^$TARGET_INDEX:"; then
-				echo "window $1 exists."
 		    tmux select-window -t "$CURRENT_SESSION:$TARGET_INDEX"
 		else
-				echo "case."
+			case $TARGET_INDEX in
+				8)
+					tmux new-window -t "$TARGET_INDEX" -n "$CURRENT_SESSION:$TARGET_INDEX"  "cd knobDistribution && nvim . && zsh"
+					;;
+				0)
+					tmux new-window -t "$TARGET_INDEX" -n "$CURRENT_SESSION:$TARGET_INDEX"  "cd results && zsh"
+					;;
+				*)
+					tmux new-window -t "$TARGET_INDEX" -n  "$CURRENT_SESSION:$TARGET_INDEX"
+					;;
+			esac
+
+		fi
+			;;
+		
+	"knuffelbeer")
+		if tmux list-windows | grep -q "^$TARGET_INDEX:"; then
+		    tmux select-window -t "$CURRENT_SESSION:$TARGET_INDEX"
+		else
 			case $TARGET_INDEX in
 				2)
 					tmux new-window -t 2 -n "$CURRENT_SESSION:2"  "vim ."
@@ -28,18 +47,31 @@ if [ "$CURRENT_SESSION" = "knuffelbeer" ]; then
 				3)
 					tmux new-window -t 3 -n "$CURRENT_SESSION:3"  "gdb ."
 					;;
+				7)
+					tmux new-window -t "$TARGET_INDEX" -n  "$CURRENT_SESSION:$7" "htop"
+					;;
 				*)
 					tmux new-window -t "$TARGET_INDEX" -n  "$CURRENT_SESSION:$TARGET_INDEX"
 					;;
 			esac
 		fi
-else
-    echo "You are not in the target session. Current session: $CURRENT_SESSION"
+			;;
+		
+
+# default configuration.
+		*)
 		if tmux list-windows | grep -q "^$TARGET_INDEX:"; then
-				echo "window $TARGET_INDEX exists."
 		    tmux select-window -t "$CURRENT_SESSION:$TARGET_INDEX"
 			else
-				echo "window $TARGET_INDEX didn't exist."
-		tmux new-window -n  "$CURRENT_SESSION:$TARGET_INDEX"
+			case $TARGET_INDEX in
+				7) # Now htop is always at window 7 no matter which session you're in.
+					tmux new-window -t "$TARGET_INDEX" -n  "$CURRENT_SESSION:$7" "htop"
+					;;
+				*)
+					tmux new-window -t "$TARGET_INDEX" -n  "$CURRENT_SESSION:$TARGET_INDEX"
+					;;
+			esac
+		# tmux new-window -n  "$CURRENT_SESSION:$TARGET_INDEX"
 	fi
-fi
+	;;
+esac
