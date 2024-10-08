a8650bdd9e7c8347c3e7ee9c54804710a3e4a12f
knuffelbeer <beerforbusiness@outlook.com>
2024-10-08 22:28:16 +0200

    hoi

--- bashScripts/git.sh
+++ bashScripts/git.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 if [[ $1 == "" ]]; then 
+	echo "submit message!"
+	read -r message 
+	if [[ $message == "" ]]; then
 	message="No message"
+	fi
 else 
 	message=$1
 fi
+
 git add .
 git commit -m "$message"
 echo "You sure?! (press y)"
@@ -14,4 +19,3 @@ git push
 else 
 	echo 'no push was made'
 fi
-
